904f2fc0cec7f48fba83c341d0912492447bc3cb
Antiez <43434488+Galsor@users.noreply.github.com>
2023-05-15 18:10:08 +0000

    Add pistacho config

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -7,10 +7,11 @@ VALIDATION_FOLDER = ROOT_FOLDER / "data" / "validation"
 
 DOCUMENT_STORE_PICKLE_PATH = DB_FOLDER / "document_store.pkl"
 ARCHIVE_FILE_NAME = "data/result.json"
+PRESTA_ARCHIVE_FILE_NAME = "data/result prestas.json"
 
 # Test sample size
 TEST_SET_SIZE = 30
-DOC_STORE_SIZE = 500
+DOC_STORE_SIZE = 3000
 
 # Embedding
 EMBEDDING_MODEL = "sentence-transformers/distiluse-base-multilingual-cased-v1"
@@ -18,20 +19,20 @@ EMBEDDING_DIM = 512
 
 # DOCUMENT STORE
 SIMILARITY_METRIC = "dot_product"
-RETRIEVED_DOCUMENTS = 5
+RETRIEVED_DOCUMENTS = 7
 
 # NLP
 SPACY_MODEL = "fr_core_news_sm"
 
 # OPENAI
-PROMPT_TEMPLATE = """Tu es l'assistant conversationnel d'une conversation de groupe de dirigeants de startups françaises nommé The Galion Project. 
-                             Ta mission est d'aider ces entrepreneurs à retrouver des réponses déjà formulées dans la conversation ou à entrer en contact les uns avec les autres.\n
-                             Tes réponses doivent être concises, efficaces, dans un language professionnel. 
-                             Tu ne dois tenir compte du message que s'il apporte des éléments de réponse à la question posée.\n
-                             Tu dois citer la source de ta réponse en faisant mention du nom de l'utilisateur et de l'ID du ou des messages qui auront servis de source.\n
-                             Si les informations contenues dans les messages ne permettent pas de répondre, mais qu'un ou plusieurs membres du groupe semble pouvoir aider, recommende à l'utilisateur de le ou les contacter explicitement.\n
-                             Si la question n'est pas adressée au groupe répond '<no response>'\n
-                             Si aucune information ne permet de répondre à la question ou de recommander une personne répond '<no response>'
+PROMPT_TEMPLATE = """Ta mission est d'aider les membres d'un groupe Telegram nommé le Galion.\n
+                             Lorsqu'un membre pose une question au groupe tu essaieras de répondre de manière étayée en te basant sur la sélection de messages ci-dessous.
+                             Tu ne dois tenir compte du message que s'il répond à la question posée.\n
+                             Pour chaque élément de réponse, tu dois citer les noms des utilisateurs qui auront fournit l'information.\n
+                             Tu ne dois pas inventer d'information supplémentaire.\n
+                             Si les informations contenues dans les messages ne permettent pas de répondre, mais qu'un ou plusieurs membres du groupe peuvent aider, recommende à l'utilisateur de le ou les contacter.\n
+                             Tu ne dois répondre qu'aux questions adressées au groupe.\n
+                             Si tu ne peux pas répondre à la question répond '<no response>'\n
                              \n\n Messages: {join(documents)} \n\n Question: {query} \n\n Ta réponse:"""
-GENERATIVE_MODEL = "text-davinci-003"
-MAX_LENGTH = 250
+GENERATIVE_MODEL = "gpt-3.5-turbo"
+MAX_LENGTH = 500
