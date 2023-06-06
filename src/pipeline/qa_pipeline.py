96c74515e2b6da957ca4e3d9eda1d55d4fe72fb5
Antiez <43434488+Galsor@users.noreply.github.com>
2023-06-06 09:19:30 +0000

    merge frozen-coconut validation results

--- src/pipeline/qa_pipeline.py
+++ src/pipeline/qa_pipeline.py
@@ -1,17 +1,10 @@
 import streamlit as st
-from haystack.nodes import Shaper
-from haystack.nodes.prompt import PromptNode
 from haystack.pipelines import Pipeline
 
 from src.pipeline.answer_check import get_veracity_node
 from src.pipeline.answer_generator import get_answer_generator
-from src.pipeline.doc_check import get_validate_documents_node
 from src.pipeline.document_store import load_document_store
-from src.pipeline.rephrase import get_answer_rephraser_node
 from src.pipeline.retriever import get_retriever
-from src.pipeline.rephrase import get_answer_rephraser_node
-from src.pipeline.check import get_validate_documents_node
-from src.pipeline.doc_duplicate import DuplicateDocQueryNode
 
 @st.cache_resource
 def load_QA_pipeline() -> Pipeline:
@@ -24,7 +17,6 @@ def load_QA_pipeline() -> Pipeline:
 
     pipe = Pipeline()
     pipe.add_node(component=retriever, name="Retriever", inputs=["Query"])
-    pipe.add_node(component=DuplicateDocQueryNode(), name="DuplicateDocQuery", inputs=["Retriever"])
-    pipe.add_node(component=generator, name="Generator", inputs=["DuplicateDocQuery"])
+    pipe.add_node(component=generator, name="Generator", inputs=["Retriever"])
     pipe.add_node(component=veracity, name="Veracity", inputs=["Generator"])
     return pipe
